bdb89d451f2c643f71ac6a9489153c9cef7d5a77
mārcis <marcis.zebergs@gmail.com>
2025-02-24 11:06:37 +0200

    ef

---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+
+let a = 5;
+let b = "5";
+ if  ( a == b){
+    console.log("a == b");
+ }
+ if(a === b){  
+    console.log("a === b");
+ }
+ let diena = parseInt(prompt("Lūdzu ievadiet dienas numuru no 1-7")); 
+  switch(diena)
+{
+    case 1: 
+        console.log("Pirmdiena ");
+        break;
+
+    case 2:
+
+        console.log("Otrdiena ");
+        break;
+    
+    case 3:
+
+        console.log("Tresdiena ");
+        break;
+
+    case 4:
+
+    console.log("Ceturtdiena ");
+    break;
+
+    case 5: 
+
+    console.log("Piekdiena ");
+    break;
+
+    case 6: 
+
+    console.log("Sesdiena  ");
+    break;
+
+    case 7: 
+
+    console.log("Svētdiena ");
+    break;
+
+
+}
