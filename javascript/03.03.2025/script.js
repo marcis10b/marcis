7c7c978fe912edc5757de0e76bfcb682d6afa18f
mārcis <marcis.zebergs@gmail.com>
2025-03-17 09:05:28 +0200

    g

---
+++ javascript/03.03.2025/script.js
@@ -0,0 +1,36 @@
+/*
+let masivs1 = [1,2,3,4,5];
+let masivs2 = [];
+
+console.log(masivs1);
+console.log(masivs1.length);
+console.log(masivs1[0]);
+console.log(masivs1[1]);
+console.log(masivs1[2]);
+console.log(masivs1[3]);
+console.log(masivs1[4]);
+let x = 4;
+console.log(masivs1[x]);
+let i = masivs1.length - 1;
+console.log(masivs1[i]);
+*/
+
+let arr = ["q", "w", "e" , "r" , "t" , "y"];
+console.log(arr);
+arr.push("u");
+
+console.log(arr);
+
+
+arr.unshift("tab");
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+
+arr[2] = null;
+console.log(arr);
